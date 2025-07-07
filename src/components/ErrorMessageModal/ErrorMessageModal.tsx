b887d468220a5f55f605a955500b5012f6206923
Michael Tu <michael_s_tu@brown.edu>
2025-07-07 14:42:21 -0400

    added more info to error modal

--- src/components/ErrorMessageModal/ErrorMessageModal.tsx
+++ src/components/ErrorMessageModal/ErrorMessageModal.tsx
@@ -54,81 +54,88 @@ export const ErrorMessageModal: React.FC<ErrorMessageModalProps> = (
   }
 
   return (
-    <div id="error-message-modal" className="mt-3 d-flex gap-3 overflow-x-auto">
+    <div id="error-message-modal" className="mt-3 d-flex flex-column overflow-x-auto p-3 rounded border border-danger border-2">
+      <h4 style={{color: 'var(--bs-danger)'}}>Could not produce a diagram</h4>
+      <p>The instance being visualized is inconsistent with the Cope and Drag spec.</p>
       {/* Parse Error Card */}
       {isSystemError && (
-        <div className="card m-auto error-card">
-          <div className="card-header bg-light">
-            <strong>
-              {systemError.type === 'parse-error' && systemError.source 
-                ? `Parse Error (${systemError.source})`
-                : 'Error'
-              }
-            </strong>
+        <>
+          <div className="card error-card">
+            <div className="card-header bg-light">
+              <strong>
+                {systemError.type === 'parse-error' && systemError.source 
+                  ? `Parse Error (${systemError.source})`
+                  : 'Error'
+                }
+              </strong>
+            </div>
+            <div className="card-body">
+              <code dangerouslySetInnerHTML={{ __html: systemError.message }}></code>
+            </div>
           </div>
-          <div className="card-body">
-            <code dangerouslySetInnerHTML={{ __html: systemError.message }}></code>
-          </div>
-        </div>
+        </>
       )}
 
       {/* Constraint Error Cards */}
       { messages && (
         <>
-          <div className="card m-auto error-card">
-            <div className="card-header bg-light">
-              <strong>In terms of CnD</strong>
+          <p><i>The graph below visualizes the localized area of the error on a valid instance with the conflicting set of constraints removed.</i></p>
+          <div className="d-flex flex-row gap-3">
+            <div className="card error-card">
+              <div className="card-header bg-light">
+                <strong>In terms of CnD</strong>
+              </div>
+              <div className="card-body">
+                Constraint: <br />
+                <code dangerouslySetInnerHTML={{__html: messages.conflictingSourceConstraint}}></code> <br />
+                conflicts with one (or some) the following source constraints: <br />
+                {[...messages.minimalConflictingConstraints.keys()].map(
+                  (key: string, index) => {
+                    console.log(
+                      `Key: ${key}, Value: ${messages.minimalConflictingConstraints.get(key)}`
+                    );
+                    return (
+                      <React.Fragment key={index}>
+                        <code
+                          onMouseEnter={addHighlightOnMouseEnter}
+                          onMouseLeave={removeHighlightOnMouseLeave}
+                          className={`error-message-${index}`}
+                          dangerouslySetInnerHTML={{__html: key }}
+                        >
+                        </code>
+                        <br />
+                      </React.Fragment>
+                    );
+                  }
+                )}
+              </div>
             </div>
-            <div className="card-body">
-              Constraint: <br />
-              <code dangerouslySetInnerHTML={{__html: messages.conflictingSourceConstraint}}></code> <br />
-              conflicts with one (or some) the following source constraints: <br />
-              {[...messages.minimalConflictingConstraints.keys()].map(
-                (key: string, index) => {
-                  console.log(
-                    `Key: ${key}, Value: ${messages.minimalConflictingConstraints.get(key)}`
-                  );
-                  return (
-                    <React.Fragment key={index}>
-                      <code
-                        onMouseEnter={addHighlightOnMouseEnter}
-                        onMouseLeave={removeHighlightOnMouseLeave}
-                        className={`error-message-${index}`}
-                        dangerouslySetInnerHTML={{__html: key }}
-                      >
-                      </code>
-                      <br />
-                    </React.Fragment>
-                  );
-                }
-              )}
-            </div>
-          </div>
 
-          <div className="card m-auto error-card">
-            <div className="card-header bg-light">
-              <strong>In terms of diagram elements</strong>
-            </div>
-            <div className="card-body">
-              Constraint: <br />{' '}
-              <code> {messages.conflictingConstraint} </code>
-              <br /> conflicts with the following constraints: <br />
-              {[...messages.minimalConflictingConstraints.values()].map(
-                (value, index1) => (
-                  <React.Fragment key={index1}>
-                    {value.map((constraint: string, index2) => (
-                      <code
-                        key={`${index1} ${index2}`}
-                        onMouseEnter={addHighlightOnMouseEnter}
-                        onMouseLeave={removeHighlightOnMouseLeave}
-                        className={`error-message-${index1}`}
-                      >
-                        {constraint}
-                      </code>
-                    ))}
-                  </React.Fragment>
-                )
-              )}
+            <div className="card error-card">
+              <div className="card-header bg-light">
+                <strong>In terms of diagram elements</strong>
+              </div>
+              <div className="card-body">
+                Constraint: <br />{' '}
+                <code> {messages.conflictingConstraint} </code>
+                <br /> conflicts with the following constraints: <br />
+                {[...messages.minimalConflictingConstraints.values()].map(
+                  (value, index1) => (
+                    <React.Fragment key={index1}>
+                      {value.map((constraint: string, index2) => (
+                        <code
+                          key={`${index1} ${index2}`}
+                          onMouseEnter={addHighlightOnMouseEnter}
+                          onMouseLeave={removeHighlightOnMouseLeave}
+                          className={`error-message-${index1}`}
+                        >
+                          {constraint}
+                        </code>
+                      ))}
+                    </React.Fragment>
+                  )
+                )}
+              </div>
             </div>
           </div>
         </>
