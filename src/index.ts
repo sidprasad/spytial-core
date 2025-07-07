28340fcbed8a8b45095a0081d195d3fab589e892
Siddhartha Prasad <siddhartha_prasad@brown.edu>
2025-07-07 10:43:47 -0400

    Adding pYUret export

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ export { JSONDataInstance, DataInstanceNormalizer } from './data-instance/json-d
 export { AlloyDataInstance, createEmptyAlloyDataInstance } from './data-instance/alloy-data-instance';
 export { DotDataInstance } from './data-instance/dot/dot-data-instance';
 export { RacketGDataInstance } from './data-instance/racket/racket-g-data-instance';
+export { PyretDataInstance } from './data-instance/pyret/pyret-data-instance';
 
 // Direct exports of key classes for convenience
 export { LayoutInstance } from './layout/layoutinstance';
